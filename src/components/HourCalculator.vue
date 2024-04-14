aa43ec321272f37fb6177501666ca3e5a60d5f78
Minhaj Sorder <74466041+minhaj57sorder@users.noreply.github.com>
2024-04-14 14:58:55 +0600

    first commit

---
+++ src/components/HourCalculator.vue
@@ -0,0 +1,55 @@
+<template>
+    <div class="hour-claculator-container">
+        <div class="hour-calculator-header">
+            <h3>Hour Calculator</h3>
+        </div>
+        <div class="title-output-container">
+            <div class="hours-minutes-outputs-titles">
+                <h5>{{ totalHours }}</h5>
+                <h5>-:-</h5>
+                <h5>{{ totalMinutes }}</h5>
+            </div>
+            <div class="hours-minutes-input-titles">
+                <h5>Hours</h5>
+                <h5>-:-</h5>
+                <h5>Minutes</h5>
+            </div>
+        </div>
+        <div class="hour-calculator-inputs-container-space">
+            <div class="hour-calculator-inputs-container">
+                <div v-for="i in totalInputLength" class="hours-minutes-input-container">
+                    <div class="hours-input"><input ref="inputHours" type="text" @input="calculate"></div>
+                    <div class="hours-minutes-separator">:</div>
+                    <div class="minutes-input"><input ref="inputMinutes" type="text" @input="calculate"></div>
+                </div>
+            </div>
+        </div>
+        <div class="add-more-input">
+            <button @click="addInput">Add input</button>
+        </div>
+    </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+// defineProps<{ msg: string }>()
+const totalInputLength = ref(8)
+const totalHours = ref(0)
+const totalMinutes = ref(0)
+const inputHours = ref([])
+const inputMinutes = ref([])
+const addInput = () => {
+    totalInputLength.value += 2
+}
+const calculate = () => {
+    console.log('Calculating...', inputHours.value[0].value, inputMinutes.value)
+    let minutes = inputMinutes.value.reduce((acc, curr) => Number(acc) + Number(curr.value), 0)
+    let hours = inputHours.value.reduce((acc, curr) => Number(acc) + Number(curr.value), 0)
+    console.log('Total hours:', hours, minutes)
+    totalHours.value = Math.floor(minutes / 60) + hours
+    totalMinutes.value = minutes % 60
+}
+// import HourCalculator from '../components/HourCalculator.vue'
+</script>
+
+<style scoped></style>
---
+++ src/pages/HourCalculatorPage.vue
@@ -0,0 +1,25 @@
+<script setup lang="ts">
+// import { ref } from 'vue'
+
+// defineProps<{ msg: string }>()
+
+// const count = ref(0)
+import HourCalculator from '../components/HourCalculator.vue'
+</script>
+
+<template>
+  <div class="calculate-hour-container">
+    <header class="calculate-hour-page-header">
+      <h2>Calculate Hours</h2>
+    </header>
+    <section>
+      <HourCalculator/>
+    </section>
+    <!-- <section>Sponsors</section> -->
+    <footer class="calculate-hour-page-footer">All rights reserved. Copyright © Hours Calculator</footer>
+  </div>
+</template>
+
+<style scoped>
+
+</style>
